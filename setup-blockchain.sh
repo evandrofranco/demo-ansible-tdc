ebeee7a2462987752bc658070650f0570ac9d0d7
Ubuntu <ubuntu@ip-172-31-17-186.us-east-2.compute.internal>
2019-04-14 20:52:11 +0000

    Correcao

--- setup-blockchain.sh
+++ setup-blockchain.sh
@@ -9,4 +9,4 @@ ansible-playbook 2_start_stop_blockchain/4.Orderer_setup.yml --inventory ansible
 ansible-playbook 2_start_stop_blockchain/5.Peers_setup.yml --inventory ansible_hosts.yml
 ansible-playbook 2_start_stop_blockchain/6.Channel1_setup.yml --inventory ansible_hosts.yml
 ansible-playbook 2_start_stop_blockchain/7.Chaincode1_setup.yml --extra-vars "chaincode_version=$CHAINCODE_VERSION" --inventory ansible_hosts.yml
-ansible-playbook 2_start_stop_blockchain/9.Chaincode_test.yml --inventory ansible_hosts.yml
+ansible-playbook 2_start_stop_blockchain/8.Chaincode_test.yml --inventory ansible_hosts.yml
